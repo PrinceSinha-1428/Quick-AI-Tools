24936d3cc5aab410ad9fb28f439872766ab9c6b3
Prince <princesinha629@gmail.com>
2025-07-12 00:21:24 +0530

    Plans and Subscription

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
 import Testimonial from '../components/Testimonial'
+import Plans from '../components/Plans'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
      <Hero/>
      <AiTools/>
      <Testimonial/>
+     <Plans/>
     </>
   )
 }
