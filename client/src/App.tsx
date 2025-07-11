5542473e026c639bc87412770fecf451075758e2
Prince <princesinha629@gmail.com>
2025-07-11 21:48:28 +0530

    Project setup client side

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { assets } from './assets/assets'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <img src={assets.logo} alt="" />
+    </div>
+  )
+}
+
+export default App
