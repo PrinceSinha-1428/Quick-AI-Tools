f9b498ad28b5b81456167e302e1860d576c6a82e
Prince <princesinha629@gmail.com>
2025-07-15 00:02:16 +0530

    Remove object

--- client/src/pages/RemoveObject.tsx
+++ client/src/pages/RemoveObject.tsx
@@ -1,12 +1,47 @@
+import { useAuth } from '@clerk/clerk-react';
+import axios from 'axios';
 import { Scissors, Sparkles } from 'lucide-react';
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL!
+
 
 const RemoveObject = () => {
-  const [input, setInput] = useState<File | null>(null);
-  const [object, setObject] = useState<string>('')
+      const [input, setInput] = useState<File | null>(null);
+      const [object, setObject] = useState<string>('')
+      const [loading, setLoading] = useState<boolean>(false);
+      const [content, setContent] = useState<string>('');
+      const {getToken} = useAuth();
       
         const onSubmitHandler = async (e:React.FormEvent<HTMLFormElement>) => {
             e.preventDefault();
+            if (!input || !(input instanceof File)) {
+            toast.error("No file selected.");
+            return;
+          }
+          try {
+          setLoading(true);
+          if(object.split(" ").length > 1){
+            return toast.error("Please enter only one object name")
+          }
+          const formdata = new FormData();
+          formdata.append('image',input);
+          formdata.append('object',object);
+          const {data} = await axios.post('/api/ai/remove-image-object',formdata,{headers:{Authorization: `Bearer ${await getToken()}`}})
+          if(data.success){
+            toast.success("Background Removed")
+            setContent(data.content);
+          }else{
+            toast.error(data.message)
+            console.log("Problem occured in data fetching")
+          }
+        } catch (error: any) {
+          toast.error(error.message)
+        } finally {
+          setLoading(false)
+        }
         }
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
@@ -24,9 +59,9 @@ const RemoveObject = () => {
            <textarea onChange={(e) => setObject(e.target.value)} value={object} rows={4}
          className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300'
           placeholder='e.g, watch or spoon , only single object name' required />
-        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-2xl cursor-pointer'>
-          <Scissors className='w-5 '/>
-          Remove Object
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-2xl cursor-pointer'>
+        {loading ? <span className='size-4 my-1 rounded-full border-2 border-t-transparent animate-spin'/> :  <Scissors className='w-5'/>}
+        {loading ? "Removing Object" : "Remove Object"}
         </button>
       </form>
       {/* right column */}
@@ -35,12 +70,17 @@ const RemoveObject = () => {
             <Scissors className='size-5 text-[#417DF6]' />
             <h1 className='text-xl font-semibold'>Processed Image</h1>
           </div>
-          <div className='flex-1 flex justify-center items-center'>
-            <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-              <Scissors className='size-9' />
-              <p>Upload an image and click "Remove Object" to get started</p>
+          {
+            !content ? (
+              <div className='flex-1 flex justify-center items-center'>
+                <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+                <Scissors className='size-9' />
+                <p>Upload an image and click "Remove Object" to get started</p>
             </div>
           </div>
+            ) : (
+              <img src={content} alt="removed background" className='mt-3 size-full' />
+            )}
       </div>
     </div>
   )
